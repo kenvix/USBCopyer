cdca58b536a80e62091346c9c2713ac6abe5d839
哈士奇 <zhangyi20100205@126.com>
2026-03-13 19:15:11 +0800

    删除dn3.5不支持的代码
    编辑器自动乱加的一大堆用不上的引用，删除即可

--- USBCopyer/FolderRuleHelper.cs
+++ USBCopyer/FolderRuleHelper.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.CodeDom;
 using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace USBCopyer
 {
